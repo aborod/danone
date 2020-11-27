fc158a1696cb719bae543289600920409b25476d
Alexander Borodin <aborod@mail.ru>
2020-11-27 15:29:20 +0300

    Test Oracle connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
 
-    echo 'Hello';
+// Подключается к XE сервису (т.е. к базе данных) на "localhost"
+$conn = oci_connect('system', 'oracle', 'heimdallr/XE');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM test_table');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+
+echo "</table>\n";
+
